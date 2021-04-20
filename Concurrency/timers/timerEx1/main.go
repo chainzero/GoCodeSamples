b0cd785e689bdc7085ff1e59bcdbe648bb3f3c9d
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-04-20 14:34:29 -0400

    adding timer example

---
+++ Concurrency/timers/timerEx1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go SlowCounter(1)
+
+	time.Sleep(15 * time.Second)
+}
+
+func SlowCounter(n int) {
+	i := 0
+	for {
+		// create a duration of n seconds
+		d := time.Duration(n) * time.Second
+
+		// create a timer for this duration
+		t := time.NewTimer(d)
+		// MewTimer provides a channel
+		<-t.C
+
+		i++
+		fmt.Println(i)
+	}
+}
