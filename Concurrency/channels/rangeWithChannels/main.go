584fe349e12e13ad5e657194907d0276a9748de3
Scott Carruthers <Iilluminorowebdev@gmail.com>
2021-04-21 13:05:38 -0400

    adding concurrency/channels example with range

---
+++ Concurrency/channels/rangeWithChannels/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+
+	//send
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+		close(ch)
+	}()
+
+	//receive
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
